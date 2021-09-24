29e70ba62e6f9c0e6f161d254e343963d4426258
Wattana <saeung2532@gmail.com>
2021-09-24 10:02:07 +0700

    20210924_00

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + "/uploaded"));
 app.use("/api/v2", require("./api"));
 
-app.listen(3001, () => {
+app.listen(8888, () => {
   console.log("Running...");
 });
